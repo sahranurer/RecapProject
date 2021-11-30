8e9dc407e67e80950936c7e7754b3ef6c9f3f2db
sahranurer <sahranurerr@gmail.com>
2021-11-30 21:23:44 +0300

    Car EntityFramework has been set, Added Brand and Color classes

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,18 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+    public interface IEntityRepository<T> where T:class,IEntity,new()
+    {
+        // GetById, GetAll, Add, Update, Delete 
+        List<T> GetAll(Expression<Func<T,bool>> filter=null);//bu yapı ile ayrı ayrı metotlar yazmaya gerek yok
+        T Get(Expression<Func<T,bool>> filter);//tek data getirmek için
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ DataAccess/Concrete/EntityFramework/RecapContext.cs
@@ -0,0 +1,21 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class RecapContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-HE88LIR;Database=RecapProject;Trusted-Connection=true");
+        }
+
+        public DbSet<Car> Cars { get; set; }
+        public DbSet<Brand> Brands { get; set; }
+        public DbSet<Color> Colors { get; set; }
+
+    }
+}
