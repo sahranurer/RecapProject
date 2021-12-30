2847459397dad596be16f4c200bfde2d9d59d17e
sahranurer <sahranurerr@gmail.com>
2021-12-30 21:58:56 +0300

    Added Validation Aspect

--- Business/Concrete/BrandManager.cs
+++ Business/Concrete/BrandManager.cs
@@ -1,5 +1,8 @@
 ﻿using Business.Abstract;
 using Business.Constans;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Autofac.Validation;
+using Core.CrossCuttingConcerns.Validation;
 using Core.Utilities;
 using DataAccess.Abstract;
 using Entities.Concrete;
@@ -18,8 +21,10 @@ namespace Business.Concrete
             _brandDal = brandDal;
         }
 
+        [ValidationAspect(typeof(BrandValidator))]
         public IResult Add(Brand brand)
         {
+        
             _brandDal.Add(brand);
             return new SuccessResult(Messages.BrandAdded);
         }
